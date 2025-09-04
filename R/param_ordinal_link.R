f883d87521e34c54369a2f8e577674b8392d325e
Jason Cory Brunson <jason.brunson@medicine.ufl.edu>
2025-09-04 14:35:09 -0400

    introduce odds_link param

--- R/param_ordinal_link.R
+++ R/param_ordinal_link.R
@@ -1,16 +1,23 @@
-#' Ordinal Regression Link Function (character)
+#' Ordinal Regression Link Functions (character)
 #'
-#' The link function of an ordinal (generalized linear) regression model.
+#' The ordinal and odds link functions of an ordinal regression model.
 #'
-#' @param values A character string of possible values. See
-#'   `values_ordinal_link` in examples below.
+#' @param values For `*_link()`, a character string from among the possible
+#'   values encoded in `values_*_link`. See the examples below.
 #'
-#' @details
-#' This parameter is used in `parsnip::set_engine('polr')`.
+#' @details These parameters are used by ordinal regression models specified by
+#'   `ordinal_reg()`, for example `parsnip::set_engine('polr')`. The
+#'   nomenclature is taken from Wurm &al (2021), who characterize the pair of
+#'   functions as a composite link function.
 #'
+#' @references Wurm, Michael J., Rathouz, Paul J., & Hanlon, Bret M. (2021).
+#'   Regularized Ordinal Regression and the ordinalNet R Package. _Journal of
+#'   Statistical Software_, 99(6), 1–42. 10.18637/jss.v099.i06
 #' @examples
 #' values_ordinal_link
 #' ordinal_link()
+#' values_odds_link
+#' odds_link()
 #' @export
 ordinal_link <- function(values = values_ordinal_link) {
   new_qual_param(
@@ -23,4 +30,30 @@ ordinal_link <- function(values = values_ordinal_link) {
 
 #' @rdname ordinal_link
 #' @export
-values_ordinal_link <- c("logistic", "probit", "loglog", "cloglog", "cauchit")
+values_ordinal_link <- c(
+  "logistic",
+  "probit",
+  "loglog",
+  "cloglog",
+  "cauchit"
+)
+
+#' @rdname ordinal_link
+#' @export
+odds_link <- function(values = values_odds_link) {
+  new_qual_param(
+    type = "character",
+    values = values,
+    label = c(odds_link = "Odds Link"),
+    finalize = NULL
+  )
+}
+
+#' @rdname ordinal_link
+#' @export
+values_odds_link <- c(
+  "cumulative_logits",
+  "adjacent_categories",
+  "continuation_ratio",
+  "stopping_ratio"
+)
